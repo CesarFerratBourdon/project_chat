fdff926545ca28e510bb4a9421baedc1dca71647
Prelang Builder <builder@prelang.com>
2016-04-05 21:23:01 +0000

    Defined validation for attribute 'email' on model 'Client'

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,4 +3,5 @@ class Client < ActiveRecord::Base
   has_many :items
   validates_formatting_of :zip_code, using: :us_zip
   validates_formatting_of :phone_number, using: :us_phone
+  validates_formatting_of :email, using: :email
 end
